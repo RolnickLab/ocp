ad5d067035dfcdd8f33e7fa95ac3a2cb1d5380dd
Victor Schmidt <vsch@pm.me>
2022-06-14 10:14:04 -0400

    towards delete tag 0

--- get_data_sample.py
+++ get_data_sample.py
@@ -10,16 +10,16 @@ Out[1]: ...
 In [2]: print(batch)
 
 """
-import torch  # noqa: F401
 import sys
 
-from ocpmodels.common.utils import (
-    build_config,
-    setup_imports,
-    setup_logging,
-)
+import matplotlib.pyplot as plt
+import numpy as np
+import torch  # noqa: F401
+from tqdm import tqdm
+
 from ocpmodels.common.flags import flags
 from ocpmodels.common.registry import registry
+from ocpmodels.common.utils import build_config, setup_imports, setup_logging
 
 if __name__ == "__main__":
 
@@ -57,11 +57,65 @@ if __name__ == "__main__":
     task = registry.get_task_class(config["mode"])(config)
     task.setup(trainer)
 
-    for batch in trainer.train_loader:
-        b = batch[0]
-        sub = torch.where(b.tags == 2)[0]
-        src_is_sub = b.edge_index[0][torch.isin(b.edge_index[0], sub)]
-        target_is_sub = b.edge_index[1][torch.isin(b.edge_index[1], sub)]
-        any_is_sub = (src_is_sub + target_is_sub) > 0
-        new_ei = b.edge_index[:, any_is_sub]
-        break
+    DELETE_TAG_0 = True
+    if DELETE_TAG_0:
+        for batch in trainer.train_loader:
+            b = batch[0]
+            batch_size = len(b.natoms)
+            non_sub = torch.where(b.tags != 0)[0]
+            src_is_not_sub = torch.isin(b.edge_index[0], non_sub)
+            target_is_not_sub = torch.isin(b.edge_index[1], non_sub)
+            neither_is_sub = src_is_not_sub * target_is_not_sub
+            new_ei = b.edge_index[:, neither_is_sub]
+            new_pos = b.pos[non_sub, :]
+            new_an = b.atomic_numbers[non_sub]
+            new_batch = b.batch[non_sub]
+            new_natoms = torch.tensor(
+                [(new_batch == i).sum() for i in range(batch_size)],
+                dtype=b.natoms.dtype,
+                device=b.natoms.device,
+            )
+            new_cell_offsets = b.cell_offsets[neither_is_sub, :]
+            new_force = b.force[non_sub, :]
+            new_fixed = b.fixed[non_sub]
+            new_tags = b.tags[non_sub]
+            new_distances = b.distances[neither_is_sub]
+            new_pos_relaxed = b.pos_relaxed[non_sub, :]
+            new_ptr = torch.tensor(
+                [0] + [b.natoms[:i].sum() for i in range(1, batch_size + 1)],
+                dtype=b.ptr.dtype,
+                device=b.ptr.device,
+            )
+            TODO = "new_neighbors and adjust atom ids in edge_index"
+            break
+
+    PLOT_TAGS = False
+    if PLOT_TAGS:
+        tags = {
+            0: [],
+            1: [],
+            2: [],
+        }
+        for batch in tqdm(trainer.train_loader):
+            for b in batch:
+                for t in tags:
+                    tags[t].append((b.tags == t).sum().item())
+
+        x = np.arange(len(tags[0]))
+        ys = [np.array(tags[t]) for t in range(3)]
+        z = np.zeros(len(x))
+        fig = plt.figure(num=1)
+        ax = fig.add_subplot(111)
+        colors = {
+            0: "b",
+            1: "y",
+            2: "g",
+        }
+        for t in tags:
+            ax.plot(x, ys[t], color=colors[t], lw=1, label=f"tag {t}")
+        for t in tags:
+            ax.fill_between(
+                x, ys[t], where=ys[t] > z, color=colors[t], interpolate=True
+            )
+        plt.legend()
+        plt.savefig("tags_dist.png", dpi=150)
