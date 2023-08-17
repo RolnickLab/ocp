86d70798b49b2d01087368362705947b2b14a206
alvaro <alvaro.carbonero.math@gmail.com>
2023-08-17 10:08:45 -0400

    Combined personalized datasets into one

--- ocpmodels/datasets/other_datasets.py
+++ ocpmodels/datasets/other_datasets.py
@@ -4,10 +4,8 @@ import pickle
 import time
 from pathlib import Path
 
-import lmdb
-import numpy as np
 import torch
-from torch_geometric.data import Data 
+from torch_geometric.data import Data, HeteroData
 
 from ocpmodels.datasets.lmdb_dataset import LmdbDataset
 from ocpmodels.common.registry import registry
@@ -132,3 +130,28 @@ class SeparateLmdbDataset(LmdbDataset): # Check that the dataset works as intend
         catalyst.total_get_time = total_get_time
 
         return (adsorbate, catalyst)
+
+@registry.register_dataset("heterogeneous")
+class HeterogeneousDataset(SeparateLmdbDataset):
+    def __getitem__(self, idx):
+        adsorbate, catalyst = super().__getitem__(idx)
+
+        reaction = HeteroData()
+        for graph in [adsorbate, catalyst]:
+            mode = graph.mode
+            for key in graph.keys:
+                if key == "edge_index":
+                    continue
+                reaction[mode][key] = graph[key]
+
+            reaction[mode, "is_close", mode].edge_index = graph.edge_index
+
+        sender = torch.repeat_interleave(torch.arange(catalyst.natoms.item()), adsorbate.natoms.item())
+        receiver = torch.arange(0, adsorbate.natoms.item()).repeat(catalyst.natoms.item())
+        reaction["catalyst", "is_disc", "adsorbate"].edge_index = torch.stack([sender, receiver])
+        reaction["catalyst", "is_disc", "adsorbate"].edge_weight = torch.repeat_interleave(
+            reaction["catalyst"].pos[:, 2],
+            adsorbate.natoms.item(),
+        )
+
+        return reaction
