7beb0ef63bf286d0f299ad12b183b41286ef206f
Ramlaoui <ramlaoui@outlook.com>
2024-04-05 13:59:00 -0400

    schedulers for equiformer_v2

--- ocpmodels/modules/scheduler.py
+++ ocpmodels/modules/scheduler.py
@@ -1,10 +1,92 @@
 """scheduler.py
 """
+
 import inspect
+import math
+from bisect import bisect
 import torch.optim.lr_scheduler as lr_scheduler
 
 from ocpmodels.common.utils import warmup_lr_lambda
 import pytorch_warmup as warmup
+
+
+def multiply(obj, num):
+    if isinstance(obj, list):
+        for i in range(len(obj)):
+            obj[i] = obj[i] * num
+    else:
+        obj = obj * num
+    return obj
+
+
+def cosine_lr_lambda(current_step: int, scheduler_params):
+    warmup_epochs = scheduler_params["warmup_epochs"]
+    lr_warmup_factor = scheduler_params["warmup_factor"]
+    max_epochs = scheduler_params["max_epochs"]
+    lr_min_factor = scheduler_params["lr_min_factor"]
+
+    # `warmup_epochs` is already multiplied with the num of iterations
+    if current_step <= warmup_epochs:
+        alpha = current_step / float(warmup_epochs)
+        return lr_warmup_factor * (1.0 - alpha) + alpha
+    else:
+        if current_step >= max_epochs:
+            return lr_min_factor
+        lr_scale = lr_min_factor + 0.5 * (1 - lr_min_factor) * (
+            1 + math.cos(math.pi * (current_step / max_epochs))
+        )
+        return lr_scale
+
+
+class CosineLRLambda:
+    def __init__(self, scheduler_params) -> None:
+        self.warmup_epochs = scheduler_params["warmup_epochs"]
+        self.lr_warmup_factor = scheduler_params["warmup_factor"]
+        self.max_epochs = scheduler_params["max_epochs"]
+        self.lr_min_factor = scheduler_params["lr_min_factor"]
+
+    def __call__(self, current_step: int):
+        # `warmup_epochs` is already multiplied with the num of iterations
+        if current_step <= self.warmup_epochs:
+            alpha = current_step / float(self.warmup_epochs)
+            return self.lr_warmup_factor * (1.0 - alpha) + alpha
+        else:
+            if current_step >= self.max_epochs:
+                return self.lr_min_factor
+            lr_scale = self.lr_min_factor + 0.5 * (1 - self.lr_min_factor) * (
+                1 + math.cos(math.pi * (current_step / self.max_epochs))
+            )
+            return lr_scale
+
+
+def multistep_lr_lambda(current_step: int, scheduler_params) -> float:
+    warmup_epochs = scheduler_params["warmup_epochs"]
+    lr_warmup_factor = scheduler_params["warmup_factor"]
+    lr_decay_epochs = scheduler_params["decay_epochs"]
+    lr_gamma = scheduler_params["decay_rate"]
+
+    if current_step <= warmup_epochs:
+        alpha = current_step / float(warmup_epochs)
+        return lr_warmup_factor * (1.0 - alpha) + alpha
+    else:
+        idx = bisect(lr_decay_epochs, current_step)
+        return pow(lr_gamma, idx)
+
+
+class MultistepLRLambda:
+    def __init__(self, scheduler_params) -> None:
+        self.warmup_epochs = scheduler_params["warmup_epochs"]
+        self.lr_warmup_factor = scheduler_params["warmup_factor"]
+        self.lr_decay_epochs = scheduler_params["decay_epochs"]
+        self.lr_gamma = scheduler_params["decay_rate"]
+
+    def __call__(self, current_step: int) -> float:
+        if current_step <= self.warmup_epochs:
+            alpha = current_step / float(self.warmup_epochs)
+            return self.lr_warmup_factor * (1.0 - alpha) + alpha
+        else:
+            idx = bisect(self.lr_decay_epochs, current_step)
+            return pow(self.lr_gamma, idx)
 
 
 class LRScheduler:
@@ -29,6 +111,18 @@ class LRScheduler:
         self.silent = silent
         if self.optim_config.get("scheduler"):
             self.scheduler_type = self.optim_config["scheduler"]
+
+        if self.scheduler_type == "LambdaLR":
+            scheduler_lambda_fn = None
+            self.lambda_type = self.optim_config["lambda_type"]
+
+            if self.lambda_type == "cosine":
+                scheduler_lambda_fn = CosineLRLambda(self.optim_config)
+            elif self.lambda_type == "multistep":
+                scheduler_lambda_fn = MultistepLRLambda(self.optim_config)
+            else:
+                raise ValueError
+            self.optim_config["lr_lambda"] = scheduler_lambda_fn
         else:
             self.scheduler_type = "LambdaLR"
 
