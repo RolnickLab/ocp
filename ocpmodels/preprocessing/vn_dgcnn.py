e22181081bd4a533ac8a3e36b4f502a97d726f11
theosaulus <theosaulus@gmail.com>
2024-04-15 10:11:59 -0400

    fixed dgcnn model - working

--- ocpmodels/preprocessing/vn_dgcnn.py
+++ ocpmodels/preprocessing/vn_dgcnn.py
@@ -17,9 +17,9 @@ from ocpmodels.preprocessing.vn_layers.vn_layers import (
     VNStdFeature,
 )
 
-
+# Adapted from Deng et al. (2021)
 class VN_dgcnn(torch.nn.Module):
-    def __init__(self, pooling="mean", n_knn=5, num_class=40, normal_channel=False):
+    def __init__(self, pooling="mean", n_knn=5, num_class=3, normal_channel=False):
         super().__init__()
         self.n_knn = n_knn       
         self.conv1 = VNLinearLeakyReLU(2, 64//3)
@@ -28,16 +28,9 @@ class VN_dgcnn(torch.nn.Module):
         self.conv4 = VNLinearLeakyReLU(128//3*2, 256//3)
 
         self.conv5 = VNLinearLeakyReLU(256//3+128//3+64//3*2, 1024//3, dim=4, share_nonlinearity=True)
-        
+        self.conv6 = VNLinearLeakyReLU(1024//3*2, 3, dim=4, share_nonlinearity=True)
+
         self.std_feature = VNStdFeature(1024//3*2, dim=4, normalize_frame=False)
-        self.linear1 = nn.Linear((1024//3)*12, 512)
-        
-        self.bn1 = nn.BatchNorm1d(512)
-        self.dp1 = nn.Dropout(p=0.5)
-        self.linear2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.dp2 = nn.Dropout(p=0.5)
-        self.linear3 = nn.Linear(256, num_class)
         
         if pooling == 'max':
             self.pool1 = VNMaxPool(64//3)
@@ -57,8 +50,10 @@ class VN_dgcnn(torch.nn.Module):
             x = x.transpose(2, 1)
 
         batch_size, num_points, _ = x.size()
+        x = x.unsqueeze(1)
+
         n_knn = min(self.n_knn, num_points - 1)
-        breakpoint()
+
         x = get_graph_feature(x, k=n_knn)
         x = self.conv1(x)
         x1 = self.pool1(x)
@@ -80,20 +75,11 @@ class VN_dgcnn(torch.nn.Module):
         
         num_points = x.size(-1)
         x_mean = x.mean(dim=-1, keepdim=True).expand(x.size())
+        
         x = torch.cat((x, x_mean), 1)
-        x, trans = self.std_feature(x)
-        x = x.view(batch_size, -1, num_points)
-        
-        x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)
-        x2 = F.adaptive_avg_pool1d(x, 1).view(batch_size, -1)
-        x = torch.cat((x1, x2), 1)
-        
-        x = F.leaky_relu(self.bn1(self.linear1(x)), negative_slope=0.2)
-        x = self.dp1(x)
-        x = F.leaky_relu(self.bn2(self.linear2(x)), negative_slope=0.2)
-        x = self.dp2(x)
-        x = self.linear3(x)
-        return x
+        x, _ = self.std_feature(x)
+        x = self.conv6(x)
+        return mean_pool(x)
     
 
 def knn(x: torch.Tensor, k: int) -> torch.Tensor:
@@ -118,28 +104,26 @@ def knn(x: torch.Tensor, k: int) -> torch.Tensor:
 
 def get_graph_feature(x, k=20, idx=None, x_coord=None):
     batch_size = x.size(0)
-    num_points = x.size(2)
+    num_points = x.size(3)
     x = x.view(batch_size, -1, num_points)
     if idx is None:
         if x_coord is None: # dynamic knn graph
             idx = knn(x, k=k)
         else:          # fixed knn graph with input point coordinates
             idx = knn(x_coord, k=k)
-    device = torch.device('cuda')
 
-    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
-
+    idx_base = torch.arange(0, batch_size).type_as(idx).view(-1, 1, 1)*num_points
     idx = idx + idx_base
-
     idx = idx.view(-1)
  
     _, num_dims, _ = x.size()
+    num_dims = num_dims // 3
 
-    x = x.transpose(2, 1).contiguous()   # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    x = x.transpose(2, 1).contiguous()
     feature = x.view(batch_size*num_points, -1)[idx, :]
-    feature = feature.view(batch_size, num_points, k, num_dims) 
-    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    feature = feature.view(batch_size, num_points, k, num_dims, 3) 
+    x = x.view(batch_size, num_points, 1, num_dims, 3).repeat(1, 1, k, 1, 1)
     
-    feature = torch.cat((feature-x, x), dim=3).permute(0, 3, 1, 2).contiguous()
+    feature = torch.cat((feature-x, x), dim=3).permute(0, 3, 4, 1, 2).contiguous()
   
     return feature
