6db1644f96a35742129bfe8a6343b4341463ed83
Lena Podina <lpodina@uwaterloo.ca>
2026-03-14 21:08:51 -0400

    data paths

--- ocpmodels/models/faenet.py
+++ ocpmodels/models/faenet.py
@@ -157,6 +157,7 @@ class EmbeddingBlock(nn.Module):
 
         # Concat period & group embedding
         if self.use_pg:
+            breakpoint()
             h_period = self.period_embedding(self.phys_emb.period[z])
             h_group = self.group_embedding(self.phys_emb.group[z])
             h = torch.cat((h, h_period, h_group), dim=1)
